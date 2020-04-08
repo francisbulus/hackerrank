29369f3f8dd3629a59b4a5cec6ac39eaba09fd65
Francis Bulus <notchera@gmail.com>
2020-04-08 13:57:29 +0100

    completed max toys challenge

---
+++ mark-toys.py
@@ -0,0 +1,11 @@
+def maximumToys(prices, k):
+    prices.sort()
+    purchase_count = 0
+    total_so_far = 0
+​
+    for toy_price in prices:
+        total_so_far += toy_price
+        if k >= total_so_far:
+            purchase_count += 1
+        else:
+            return purchase_count
