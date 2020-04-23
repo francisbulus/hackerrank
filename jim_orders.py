7cc867d64c2478d6d18ec9c3140db469ccf84f70
francisbulus <notchera@gmail.com>
2020-04-23 22:27:05 +0100

    _

--- jim_orders.py
+++ jim_orders.py
@@ -2,5 +2,5 @@ def jimOrders(orders):
     dict = {}
     for k in range(1,(len(orders)+1)):
         dict[k] = sum(orders[k-1])
-    order_of_orders = sorted(dict, key=dict.get)
-    return order_of_orders
+    order = sorted(dict, key=dict.get)
+    return order
